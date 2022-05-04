84ba15f253a7b1931eb6dd1f2278696bdbd26fe7
Sanika.Darji <Sanika.Darji@CORE-LT-086.coreintegra.in>
2022-05-04 15:12:12 +0530

    second commit

---
+++ demogit/src/demogit/Test.java
@@ -0,0 +1,11 @@
+package demogit;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi this is my new git");
+
+	}
+
+}
